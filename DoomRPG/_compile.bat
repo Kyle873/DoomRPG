7b6cc5d52734baa7345eb47b83e1234fba42c618
Kyle Belanger <kyle873@gmail.com>
2013-07-17 03:19:53 -0400

    Initial commit

---
+++ DoomRPG/_compile.bat
@@ -0,0 +1,17 @@
+@echo off
+color F
+
+set PATH=C:\Games\Doom\ACC
+set SRC=.\scripts
+set OBJ=.\acs
+
+acc -i %SRC% %SRC%\Accessories %OBJ%\Accessories
+acc -i %SRC% %SRC%\Menu %OBJ%\Menu
+acc -i %SRC% %SRC%\RPG %OBJ%\RPG
+acc -i %SRC% %SRC%\Shield %OBJ%\Shield
+acc -i %SRC% %SRC%\Shop %OBJ%\Shop
+acc -i %SRC% %SRC%\Skills %OBJ%\Skills
+acc -i %SRC% %SRC%\Stats %OBJ%\Stats
+acc -i %SRC% %SRC%\Utils %OBJ%\Utils
+
+pause >nul
