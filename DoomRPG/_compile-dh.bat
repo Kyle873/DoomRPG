7b53eff33753b6c086ba45bad61a3d0e17505cac
Kyle Belanger <kyle873@gmail.com>
2013-08-02 19:55:44 -0400

    More work on converting ACS to DS

--- DoomRPG/_compile-dh.bat
+++ DoomRPG/_compile-dh.bat
@@ -5,8 +5,9 @@ set PATH=C:\Games\Doom\DH-ACC
 set SRC=.\scripts
 set OBJ=.\acs
 
-dh-acc -Z -i %SRC% Menu.ds %OBJ%\Menu.o --named-scripts
-dh-acc -Z -i %SRC% Stats.ds %OBJ%\Stats.o --named-scripts
 dh-acc -Z -i %SRC% RPG.ds %OBJ%\RPG.o --named-scripts
+dh-acc -Z -i %SRC% Stats.ds %OBJ%\Stats.o --named-scripts
+dh-acc -Z -i %SRC% Skills.ds %OBJ%\Skills.o --named-scripts
+dh-acc -Z -i %SRC% Menu.ds %OBJ%\Menu.o --named-scripts
 
 pause >nul
