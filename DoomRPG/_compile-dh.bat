f0d18876acb7d5a4e6d1df22699e0ee7c03f21ad
Kyle Belanger <kyle873@gmail.com>
2013-08-02 17:20:45 -0400

    More work moving from ACS to DamnScript

--- DoomRPG/_compile-dh.bat
+++ DoomRPG/_compile-dh.bat
@@ -5,8 +5,8 @@ set PATH=C:\Games\Doom\DH-ACC
 set SRC=.\scripts
 set OBJ=.\acs
 
-dh-acc -Z -i %SRC% Menu.ds %OBJ%\Menu.o
-dh-acc -Z -i %SRC% Stats.ds %OBJ%\Stats.o
-dh-acc -Z -i %SRC% RPG.ds %OBJ%\RPG.o
+dh-acc -Z -i %SRC% Menu.ds %OBJ%\Menu.o --named-scripts
+dh-acc -Z -i %SRC% Stats.ds %OBJ%\Stats.o --named-scripts
+dh-acc -Z -i %SRC% RPG.ds %OBJ%\RPG.o --named-scripts
 
 pause >nul
