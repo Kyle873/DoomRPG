df1e3cb849a46ad1e75e8848299b82f719346642
Kyle873 <kyle873@gmail.com>
2014-02-09 17:20:52 -0500

    Turns out that bug wasn't really a bug after all

--- Utilities/DoomRPGStrengthWriter.cs
+++ Utilities/DoomRPGStrengthWriter.cs
@@ -7,12 +7,12 @@ namespace DoomRPG
 	{
 		public static void Main(string[] Args)
 		{
-			float strength = 0.01f;
+			float strength = 1.01f;
             TextWriter output = File.CreateText("Strength.txt");
 			
             for (int i = 1; i <= 32000; i++)
 			{
-                string item = "actor Strength" + i + " : Strength { DamageFactor " + string.Format("{0:0.00}", strength) + " }";
+                string item = "actor Strength" + i + " : Strength { DamageFactor \"Normal\", " + string.Format("{0:0.00}", strength) + " }";
                 Console.WriteLine(item);
                 output.WriteLine(item);
 				strength += 0.01f;
