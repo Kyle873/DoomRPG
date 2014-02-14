f2805b26648ff3cd45f8171f5194e19861121d5d
Kyle873 <kyle873@gmail.com>
2014-02-14 13:01:07 -0500

    Finished Skills, Renamed Main to RPG, moved some stuff around, full compile works now without any errors

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -9,8 +9,9 @@ rem _timestamp
 
 DH-acc -Z -i %SRC%\inc %SRC%\Arena.ds -o %OBJ%\Arena.o
 DH-acc -Z -i %SRC%\inc %SRC%\Augs.ds -o %OBJ%\Augs.o
-DH-acc -Z -i %SRC%\inc %SRC%\Main.ds -o %OBJ%\Main.o
 DH-acc -Z -i %SRC%\inc %SRC%\Minigame.ds -o %OBJ%\Minigame.o
+DH-acc -Z -i %SRC%\inc %SRC%\RPG.ds -o %OBJ%\RPG.o
+DH-acc -Z -i %SRC%\inc %SRC%\Skills.ds -o %OBJ%\Skills.o
 DH-acc -Z -i %SRC%\inc %SRC%\Stats.ds -o %OBJ%\Stats.o
 DH-acc -Z -i %SRC%\inc %SRC%\Stims.ds -o %OBJ%\Stims.o
 DH-acc -Z -i %SRC%\inc %SRC%\Utils.ds -o %OBJ%\Utils.o
