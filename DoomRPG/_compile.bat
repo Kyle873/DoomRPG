4ce6f9ac6bebd1b46c841978f8d16d0da6655e16
Kyle873 <kyle873@gmail.com>
2014-02-14 11:47:13 -0500

    Implemented Arena, Augs, Stats, Stims and some of Utils

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -1,22 +1,19 @@
 @echo off
 color F
 
-set PATH=C:\Games\Doom\ACC
+set PATH=..\..\\DH-ACC
 set SRC=.\scripts
 set OBJ=.\acs
 
-_timestamp
+rem _timestamp
 
-acc -i %SRC% %SRC%\Augs %OBJ%\Augs
-acc -i %SRC% %SRC%\Arena %OBJ%\Arena
-acc -i %SRC% %SRC%\Menu %OBJ%\Menu
-acc -i %SRC% %SRC%\Minigame %OBJ%\Minigame
-acc -i %SRC% %SRC%\RPG %OBJ%\RPG
-acc -i %SRC% %SRC%\Shield %OBJ%\Shield
-acc -i %SRC% %SRC%\Shop %OBJ%\Shop
-acc -i %SRC% %SRC%\Skills %OBJ%\Skills
-acc -i %SRC% %SRC%\Stats %OBJ%\Stats
-acc -i %SRC% %SRC%\Stims %OBJ%\Stims
-acc -i %SRC% %SRC%\Utils %OBJ%\Utils
+DH-acc -Z -i %SRC%\inc %SRC%\Arena.ds -o %OBJ%\Arena.o
+DH-acc -Z -i %SRC%\inc %SRC%\Augs.ds -o %OBJ%\Augs.o
+DH-acc -Z -i %SRC%\inc %SRC%\Main.ds -o %OBJ%\Main.o
+DH-acc -Z -i %SRC%\inc %SRC%\Stats.ds -o %OBJ%\Stats.o
+DH-acc -Z -i %SRC%\inc %SRC%\Stims.ds -o %OBJ%\Stims.o
+DH-acc -Z -i %SRC%\inc %SRC%\Utils.ds -o %OBJ%\Utils.o
+
+echo Done!
 
 pause >nul
