900a3eae76b773522957fb73b58ca211ff450e77
Kyle873 <kyle873@gmail.com>
2014-02-14 14:28:52 -0500

    Finished Shop

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -11,6 +11,7 @@ DH-acc -Z -i %SRC%\inc %SRC%\Arena.ds -o %OBJ%\Arena.o
 DH-acc -Z -i %SRC%\inc %SRC%\Augs.ds -o %OBJ%\Augs.o
 DH-acc -Z -i %SRC%\inc %SRC%\Minigame.ds -o %OBJ%\Minigame.o
 DH-acc -Z -i %SRC%\inc %SRC%\RPG.ds -o %OBJ%\RPG.o
+DH-acc -Z -i %SRC%\inc %SRC%\Shop.ds -o %OBJ%\Shop.o
 DH-acc -Z -i %SRC%\inc %SRC%\Skills.ds -o %OBJ%\Skills.o
 DH-acc -Z -i %SRC%\inc %SRC%\Stats.ds -o %OBJ%\Stats.o
 DH-acc -Z -i %SRC%\inc %SRC%\Stims.ds -o %OBJ%\Stims.o
