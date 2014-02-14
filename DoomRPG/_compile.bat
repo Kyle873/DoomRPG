e9cf70d8c59ce8ea5f239a3d60c3ab4d40ef8462
Kyle873 <kyle873@gmail.com>
2014-02-14 15:28:49 -0500

    Added --named-scripts

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -7,16 +7,16 @@ set OBJ=.\acs
 
 rem _timestamp
 
-DH-acc -Z -i %SRC%\inc %SRC%\Arena.ds -o %OBJ%\Arena.o
-DH-acc -Z -i %SRC%\inc %SRC%\Augs.ds -o %OBJ%\Augs.o
-DH-acc -Z -i %SRC%\inc %SRC%\Menu.ds -o %OBJ%\Menu.o
-DH-acc -Z -i %SRC%\inc %SRC%\Minigame.ds -o %OBJ%\Minigame.o
-DH-acc -Z -i %SRC%\inc %SRC%\RPG.ds -o %OBJ%\RPG.o
-DH-acc -Z -i %SRC%\inc %SRC%\Shop.ds -o %OBJ%\Shop.o
-DH-acc -Z -i %SRC%\inc %SRC%\Skills.ds -o %OBJ%\Skills.o
-DH-acc -Z -i %SRC%\inc %SRC%\Stats.ds -o %OBJ%\Stats.o
-DH-acc -Z -i %SRC%\inc %SRC%\Stims.ds -o %OBJ%\Stims.o
-DH-acc -Z -i %SRC%\inc %SRC%\Utils.ds -o %OBJ%\Utils.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Arena.ds -o %OBJ%\Arena.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Augs.ds -o %OBJ%\Augs.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Menu.ds -o %OBJ%\Menu.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Minigame.ds -o %OBJ%\Minigame.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\RPG.ds -o %OBJ%\RPG.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Shop.ds -o %OBJ%\Shop.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Skills.ds -o %OBJ%\Skills.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Stats.ds -o %OBJ%\Stats.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Stims.ds -o %OBJ%\Stims.o
+DH-acc --named-scripts -Z -i %SRC%\inc %SRC%\Utils.ds -o %OBJ%\Utils.o
 
 echo Done!
 
