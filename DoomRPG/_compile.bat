bb6aff8bcac82ceb584f9e24629ae903238ad6d5
Kyle873 <kyle873@gmail.com>
2014-02-14 22:05:58 -0500

    DAAAAAAAAAAAVIIIIIIIIIIIIIID!!!

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -7,17 +7,17 @@ set OBJ=.\acs
 
 rem _timestamp
 
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Arena.ds -o %OBJ%\Arena.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Augs.ds -o %OBJ%\Augs.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Menu.ds -o %OBJ%\Menu.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Minigame.ds -o %OBJ%\Minigame.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\RPG.ds -o %OBJ%\RPG.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Shield.ds -o %OBJ%\Shield.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Shop.ds -o %OBJ%\Shop.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Skills.ds -o %OBJ%\Skills.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Stats.ds -o %OBJ%\Stats.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Stims.ds -o %OBJ%\Stims.o
-DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log -Z -i %SRC%\inc %SRC%\Utils.ds -o %OBJ%\Utils.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Arena.ds -o %OBJ%\Arena.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Augs.ds -o %OBJ%\Augs.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Menu.ds -o %OBJ%\Menu.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Minigame.ds -o %OBJ%\Minigame.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\RPG.ds -o %OBJ%\RPG.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Shield.ds -o %OBJ%\Shield.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Shop.ds -o %OBJ%\Shop.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Skills.ds -o %OBJ%\Skills.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Stats.ds -o %OBJ%\Stats.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Stims.ds -o %OBJ%\Stims.o
+DH-acc --named-scripts --auto-stack-size=0 --debug-mapregister-list=MapVars.log --debug-maparray-list=MapArrays.log -Z -i %SRC%\inc %SRC%\Utils.ds -o %OBJ%\Utils.o
 
 echo Done!
 
