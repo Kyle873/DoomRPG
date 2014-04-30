a16876b6b299204088baf9966661c0af6ca81fab
Kyle873 <kyle873@gmail.com>
2014-04-30 01:12:59 -0400

    Use new build script created in the menurewrite branch

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -8,26 +8,52 @@ set ACC=DH-acc --named-scripts --use-chunk-ATAG --debug-error-pos --debug-mapreg
 
 _timestamp
 
+echo ----------------------------------------
+
 rem Standard Libraries
-%ACC% -c %PATH%/lib/stdlib.ds -o %PATH%\lib\stdlib.o
-%ACC% -c %PATH%/lib/stdio.ds -o %PATH%\lib\stdio.o
-%ACC% %PATH%\lib\stdlib.o %PATH%\lib\stdio.o -o %OBJ%\libDS.o
+echo Compiling Standard Libraries
+%ACC% -c %PATH%/lib/stdlib.ds -o %OBJ%\stdlib.o
+%ACC% -c %PATH%/lib/stdio.ds -o %OBJ%\stdio.o
+%ACC% -c %PATH%/lib/string.ds -o %OBJ%\string.o
 
 rem Doom RPG Modules
-%ACC% %SRC%\Arena.ds -o %OBJ%\Arena.o
-%ACC% %SRC%\Augs.ds -o %OBJ%\Augs.o
-%ACC% %SRC%\GUI.ds -o %OBJ%\GUI.o
-%ACC% %SRC%\Menu.ds -o %OBJ%\Menu.o
-%ACC% %SRC%\Minigame.ds -o %OBJ%\Minigame.o
-%ACC% %SRC%\Outpost.ds -o %OBJ%\Outpost.o
-%ACC% %SRC%\RPG.ds -o %OBJ%\RPG.o
-%ACC% %SRC%\Shield.ds -o %OBJ%\Shield.o
-%ACC% %SRC%\Shop.ds -o %OBJ%\Shop.o
-%ACC% %SRC%\Skills.ds -o %OBJ%\Skills.o
-%ACC% %SRC%\Stats.ds -o %OBJ%\Stats.o
-%ACC% %SRC%\Stims.ds -o %OBJ%\Stims.o
-%ACC% %SRC%\Utils.ds -o %OBJ%\Utils.o
+echo Compiling Arena
+%ACC% -c %SRC%\Arena.ds -o %OBJ%\Arena.o
+echo Compiling Augs
+%ACC% -c %SRC%\Augs.ds -o %OBJ%\Augs.o
+echo Compiling GUI
+%ACC% -c %SRC%\GUI.ds -o %OBJ%\GUI.o
+echo Compiling Menu
+%ACC% -c %SRC%\Menu.ds -o %OBJ%\Menu.o
+echo Compiling MenuGUI
+%ACC% -c %SRC%\MenuGUI.ds -o %OBJ%\MenuGUI.o
+echo Compiling Minigame
+%ACC% -c %SRC%\Minigame.ds -o %OBJ%\Minigame.o
+echo Compiling Outpost
+%ACC% -c %SRC%\Outpost.ds -o %OBJ%\Outpost.o
+echo Compiling RPG
+%ACC% -c %SRC%\RPG.ds -o %OBJ%\RPG.o
+echo Compiling Shield
+%ACC% -c %SRC%\Shield.ds -o %OBJ%\Shield.o
+echo Compiling Shop
+%ACC% -c %SRC%\Shop.ds -o %OBJ%\Shop.o
+echo Compiling Skills
+%ACC% -c %SRC%\Skills.ds -o %OBJ%\Skills.o
+echo Compiling Stats
+%ACC% -c %SRC%\Stats.ds -o %OBJ%\Stats.o
+echo Compiling Stims
+%ACC% -c %SRC%\Stims.ds -o %OBJ%\Stims.o
+echo Compiling Utils
+%ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
+echo Linking RPG.lib
+%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\Menu.o %OBJ%\MenuGUI.o %OBJ%\Minigame.o %OBJ%\Outpost.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
+echo ----------------------------------------
+
+echo Cleaning up object files
+del %OBJ%\*.o
+
+echo ----------------------------------------
 echo Done!
 
 pause >nul
