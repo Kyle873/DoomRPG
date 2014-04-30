4a571b4b6759905d14f0d21d05612d20b5c33641
Kyle873 <kyle873@gmail.com>
2014-04-30 01:15:09 -0400

    Fiees to the build file for the master branch

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -25,8 +25,6 @@ echo Compiling GUI
 %ACC% -c %SRC%\GUI.ds -o %OBJ%\GUI.o
 echo Compiling Menu
 %ACC% -c %SRC%\Menu.ds -o %OBJ%\Menu.o
-echo Compiling MenuGUI
-%ACC% -c %SRC%\MenuGUI.ds -o %OBJ%\MenuGUI.o
 echo Compiling Minigame
 %ACC% -c %SRC%\Minigame.ds -o %OBJ%\Minigame.o
 echo Compiling Outpost
@@ -46,7 +44,7 @@ echo Compiling Stims
 echo Compiling Utils
 %ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
 echo Linking RPG.lib
-%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\Menu.o %OBJ%\MenuGUI.o %OBJ%\Minigame.o %OBJ%\Outpost.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
+%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Outpost.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
 echo ----------------------------------------
 
