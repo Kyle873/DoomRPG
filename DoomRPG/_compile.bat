ace69a957e4dcf22eb13ddd2a9e9490b753484ff
Kyle873 <kyle873@gmail.com>
2014-06-17 13:51:41 -0400

    Draft of the Password System

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -35,6 +35,8 @@ echo Compiling Monsters
 %ACC% -c %SRC%\Monsters.ds -o %OBJ%\Monsters.o
 echo Compiling Outpost
 %ACC% -c %SRC%\Outpost.ds -o %OBJ%\Outpost.o
+echo Compiling Password
+%ACC% -c %SRC%\Password.ds -o %OBJ%\Password.o
 echo Compiling Popoffs
 %ACC% -c %SRC%\Popoffs.ds -o %OBJ%\Popoffs.o
 echo Compiling RPG
@@ -52,7 +54,7 @@ echo Compiling Stims
 echo Compiling Utils
 %ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
 echo Linking RPG.lib
-%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\HealthBars.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Monsters.o %OBJ%\Outpost.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
+%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\HealthBars.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Monsters.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
 echo ----------------------------------------
 
