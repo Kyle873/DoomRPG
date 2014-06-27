83a39e126a8000a458230d434cdb684777b888ab
Kyle873 <kyle873@gmail.com>
2014-06-26 20:36:48 -0400

    Mission System: Started, still needs cleaning, lots of placeholder and messy stuff, etc.

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -37,8 +37,8 @@ echo Compiling Menu
 %ACC% -c %SRC%\Menu.ds -o %OBJ%\Menu.o
 echo Compiling Minigame
 %ACC% -c %SRC%\Minigame.ds -o %OBJ%\Minigame.o
-echo Compiling Missions
-%ACC% -c %SRC%\Missions.ds -o %OBJ%\Missions.o
+echo Compiling Mission
+%ACC% -c %SRC%\Mission.ds -o %OBJ%\Mission.o
 echo Compiling Monsters
 %ACC% -c %SRC%\Monsters.ds -o %OBJ%\Monsters.o
 echo Compiling Outpost
@@ -62,7 +62,7 @@ echo Compiling Stims
 echo Compiling Utils
 %ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
 echo Linking RPG.lib
-%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Missions.o %OBJ%\Monsters.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
+%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
 echo ----------------------------------------
 
