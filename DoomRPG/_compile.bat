d9737a12f5cc959c199c31e7040fd0f449f6e110
Kyle873 <kyle873@gmail.com>
2014-07-06 13:14:27 -0400

    Moved all progress made to the GUI from menurewrite into master and re-integrated it, somehow drawing broke during this process

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -27,6 +27,8 @@ echo Compiling Augs
 %ACC% -c %SRC%\Augs.ds -o %OBJ%\Augs.o
 echo Compiling GUI
 %ACC% -c %SRC%\GUI.ds -o %OBJ%\GUI.o
+echo Compiling GUIMenu
+%ACC% -c %SRC%\GUIMenu.ds -o %OBJ%\GUIMenu.o
 echo Compiling HealthBars
 %ACC% -c %SRC%\HealthBars.ds -o %OBJ%\HealthBars.o
 echo Compiling HUD
@@ -64,7 +66,7 @@ echo Compiling Stims
 echo Compiling Utils
 %ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
 echo Linking RPG.lib
-%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Namegen.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
+%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\GUI.o %OBJ%\GUIMenu.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Namegen.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
 echo ----------------------------------------
 
