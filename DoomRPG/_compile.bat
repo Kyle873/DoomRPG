e112d2eee4178ea8d8bfa88532b70f7ec6e3010e
Kyle873 <kyle873@gmail.com>
2014-07-23 10:50:37 -0400

    Added more stuff to TODO, potential fix for Reinforcement monsters not getting stats, cosmetic changes to build script

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -8,74 +8,75 @@ set ACC=DH-acc --named-scripts --near-pointers --use-chunk-ATAG --debug-error-po
 
 _timestamp
 
-echo ----------------------------------------
-
 rem Standard Libraries
 echo Compiling Standard Libraries
+echo    - stdlib
 %ACC% -c %PATH%/lib/stdlib.ds -o %OBJ%\stdlib.o
+echo    - stdio
 %ACC% -c %PATH%/lib/stdio.ds -o %OBJ%\stdio.o
+echo    - string
 %ACC% -c %PATH%/lib/string.ds -o %OBJ%\string.o
 
 rem Doom RPG Libraries
-echo Compiling BitIO
+echo Compiling Doom RPG Libraries
+echo    - BitIO
 %ACC% -c %SRC%/lib/BitIO.c -o %OBJ%\BitIO.o
 
 rem Doom RPG Modules
-echo Compiling Arena
+echo Compiling Doom RPG
+echo    - Arena
 %ACC% -c %SRC%\Arena.ds -o %OBJ%\Arena.o
-echo Compiling Augs
+echo    - Augs
 %ACC% -c %SRC%\Augs.ds -o %OBJ%\Augs.o
-rem echo Compiling GUI
+rem echo    - GUI
 rem %ACC% -c %SRC%\GUI.ds -o %OBJ%\GUI.o
-rem echo Compiling GUIMenu
+rem echo    - GUIMenu
 rem %ACC% -c %SRC%\GUIMenu.ds -o %OBJ%\GUIMenu.o
-echo Compiling HealthBars
+echo    - HealthBars
 %ACC% -c %SRC%\HealthBars.ds -o %OBJ%\HealthBars.o
-echo Compiling HUD
+echo    - HUD
 %ACC% -c %SRC%\HUD.ds -o %OBJ%\HUD.o
-echo Compiling ItemData
+echo    - ItemData
 %ACC% -c %SRC%\ItemData.ds -o %OBJ%\ItemData.o
-echo Compiling Map
+echo    - Map
 %ACC% -c %SRC%\Map.ds -o %OBJ%\Map.o
-echo Compiling Menu
+echo    - Menu
 %ACC% -c %SRC%\Menu.ds -o %OBJ%\Menu.o
-rem echo Compiling Minigame
+rem echo    - Minigame
 rem %ACC% -c %SRC%\Minigame.ds -o %OBJ%\Minigame.o
-echo Compiling Mission
+echo    - Mission
 %ACC% -c %SRC%\Mission.ds -o %OBJ%\Mission.o
-echo Compiling Monsters
+echo    - Monsters
 %ACC% -c %SRC%\Monsters.ds -o %OBJ%\Monsters.o
-echo Compiling Namegen
+echo    - Namegen
 %ACC% -c %SRC%\Namegen.ds -o %OBJ%\Namegen.o
-echo Compiling Outpost
+echo    - Outpost
 %ACC% -c %SRC%\Outpost.ds -o %OBJ%\Outpost.o
-echo Compiling Password
+echo    - Password
 %ACC% -c %SRC%\Password.ds -o %OBJ%\Password.o
-echo Compiling Popoffs
+echo    - Popoffs
 %ACC% -c %SRC%\Popoffs.ds -o %OBJ%\Popoffs.o
-echo Compiling RPG
+echo    - RPG
 %ACC% -c %SRC%\RPG.ds -o %OBJ%\RPG.o
-echo Compiling Shield
+echo    - Shield
 %ACC% -c %SRC%\Shield.ds -o %OBJ%\Shield.o
-echo Compiling Shop
+echo    - Shop
 %ACC% -c %SRC%\Shop.ds -o %OBJ%\Shop.o
-echo Compiling Skills
+echo    - Skills
 %ACC% -c %SRC%\Skills.ds -o %OBJ%\Skills.o
-echo Compiling Stats
+echo    - Stats
 %ACC% -c %SRC%\Stats.ds -o %OBJ%\Stats.o
-echo Compiling Stims
+echo    - Stims
 %ACC% -c %SRC%\Stims.ds -o %OBJ%\Stims.o
-echo Compiling Utils
+echo    - Utils
 %ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
+
 echo Linking RPG.lib
 %ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\ItemData.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Namegen.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
-echo ----------------------------------------
-
-echo Cleaning up object files
+echo Cleaning up Object Files
 del %OBJ%\*.o
 
-echo ----------------------------------------
 echo Done!
 
 pause >nul
