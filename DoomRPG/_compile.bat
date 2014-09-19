c2b4c64107e5486727691d3bc64086374a3dcf78
Kyle873 <kyle873@gmail.com>
2014-09-19 14:19:02 -0400

    Get rid of old Minigame stuff, implement new Item Roulette minigame

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -40,8 +40,8 @@ echo    - Map
 %ACC% -c %SRC%\Map.ds -o %OBJ%\Map.o
 echo    - Menu
 %ACC% -c %SRC%\Menu.ds -o %OBJ%\Menu.o
-rem echo    - Minigame
-rem %ACC% -c %SRC%\Minigame.ds -o %OBJ%\Minigame.o
+echo    - Minigame
+%ACC% -c %SRC%\Minigame.ds -o %OBJ%\Minigame.o
 echo    - Mission
 %ACC% -c %SRC%\Mission.ds -o %OBJ%\Mission.o
 echo    - Monsters
@@ -70,7 +70,7 @@ echo    - Utils
 %ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
 
 echo Linking RPG.lib
-%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\ItemData.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Namegen.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
+%ACC% %OBJ%\stdlib.o %OBJ%\stdio.o %OBJ%\string.o %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\ItemData.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Namegen.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
 echo Cleaning up Object Files
 del %OBJ%\*.o
