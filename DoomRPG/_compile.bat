9683415155aa94d71e57e953dbd16d4c109baaf4
Kyle873 <kyle873@gmail.com>
2014-11-13 00:03:38 -0500

    New character save data format and preperations to implement it

--- DoomRPG/_compile.bat
+++ DoomRPG/_compile.bat
@@ -34,6 +34,8 @@ echo    - Arena
 %ACC% -c %SRC%\Arena.ds -o %OBJ%\Arena.o
 echo    - Augs
 %ACC% -c %SRC%\Augs.ds -o %OBJ%\Augs.o
+echo    - CharSave
+%ACC% -c %SRC%\CharSave.ds -o %OBJ%\CharSave.o
 rem echo    - GUI
 rem %ACC% -c %SRC%\GUI.ds -o %OBJ%\GUI.o
 rem echo    - GUIMenu
@@ -58,8 +60,8 @@ echo    - Namegen
 %ACC% -c %SRC%\Namegen.ds -o %OBJ%\Namegen.o
 echo    - Outpost
 %ACC% -c %SRC%\Outpost.ds -o %OBJ%\Outpost.o
-echo    - Password
-%ACC% -c %SRC%\Password.ds -o %OBJ%\Password.o
+echo    - CharSave
+%ACC% -c %SRC%\CharSave.ds -o %OBJ%\CharSave.o
 echo    - Popoffs
 %ACC% -c %SRC%\Popoffs.ds -o %OBJ%\Popoffs.o
 echo    - RPG
@@ -80,7 +82,7 @@ echo    - Utils
 %ACC% -c %SRC%\Utils.ds -o %OBJ%\Utils.o
 
 echo Linking RPG.lib
-%ACC% %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\ItemData.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Namegen.o %OBJ%\Outpost.o %OBJ%\Password.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Turret.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
+%ACC% %OBJ%\BitIO.o %OBJ%\Arena.o %OBJ%\Augs.o %OBJ%\HealthBars.o %OBJ%\HUD.o %OBJ%\ItemData.o %OBJ%\Map.o %OBJ%\Menu.o %OBJ%\Minigame.o %OBJ%\Mission.o %OBJ%\Monsters.o %OBJ%\Namegen.o %OBJ%\Outpost.o %OBJ%\CharSave.o %OBJ%\Popoffs.o %OBJ%\RPG.o %OBJ%\Shield.o %OBJ%\Shop.o %OBJ%\Skills.o %OBJ%\Stats.o %OBJ%\Stims.o %OBJ%\Turret.o %OBJ%\Utils.o -o %OBJ%\RPG.lib
 
 echo Cleaning up Object Files
 del %OBJ%\*.o
